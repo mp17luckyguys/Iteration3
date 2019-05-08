b238afa25a509b932bb640efaad6b104627f9276
Zekun Di <zdii0001@student.monash.edu>
2019-05-08 22:25:23 +1000

    events

--- FamilyGo/FamilyGo/App_Start/BundleConfig.cs
+++ FamilyGo/FamilyGo/App_Start/BundleConfig.cs
@@ -26,6 +26,8 @@ namespace FamilyGo
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            bundles.Add(new ScriptBundle("~/bundles/datepicker").Include(
+          ));
         }
     }
 }
--- FamilyGo/FamilyGo/Controllers/EventController.cs
+++ FamilyGo/FamilyGo/Controllers/EventController.cs
@@ -1,55 +1,88 @@
-﻿using Newtonsoft.Json;
+﻿using FamilyGo.Models;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 
 namespace FamilyGo.Controllers
 {
     public class EventController : Controller
     {
-
+        private FamilyGoiteration2_dbEntities1 db = new FamilyGoiteration2_dbEntities1();
         // GET: Event
         public ActionResult Index()
         {
             return View();
         }
-        public ActionResult GetEvents()
+        public ActionResult GetEvents(string isFree, string point, string start_time, string end_time, string category)
         {
+            string basicUrl = "http://api.eventfinda.com.au/v2/events.json?rows=20&order=date";
+            Dictionary<String, String> categoryDic = new Dictionary<string, string>();
+            categoryDic.Add("1", "11");
+            categoryDic.Add("2", "73,192,1,48,22,60,304,23,251,253,320");
+            categoryDic.Add("3","5,52,49,305,140,205");
+            categoryDic.Add("4", "30,205");
+            categoryDic.Add("5", "275,163");
+            categoryDic.Add("6", "321,25");
+            categoryDic.Add("7", "126");
+            if (category != "0")
+            { basicUrl = basicUrl + "&category=" + categoryDic[category]; }
+            else { basicUrl = basicUrl + "&category=23,304,25,22,48,320,251,253,60,11,73,5,52,30,321,49,192,305,275,163,140,205,216"; }
+            if (point != "0")
+            { basicUrl = basicUrl + "&radius=10&point=" + point; }
+            else { basicUrl = basicUrl + "&radius=100&point=-37.813611,144.963056"; }
+            if (isFree != "0")
+            { basicUrl = basicUrl + "&free=" + isFree; }
+            if (start_time != "0")
+            { basicUrl = basicUrl + "&start_date=" + start_time; }
+            if (end_time != "0")
+            { basicUrl = basicUrl + "&end_date=" + end_time; }
             /*
-             * category: <id>11</id>< name > Exhibitions </ name >
+             * category: 
+             * exhibitions 1
+             * <id>11</id>< name > Exhibitions </ name >
+             * shows 2
              * <id>73</id>< name >< ![CDATA[A & P Shows, Field Days]] ></ name >
-             * <id>5</id><name>Children, Kids, Holidays</name>
-             * <id>52</id><name>Family Entertainment</name>
-             * <id>30</id><name>Festivals</name>
-             * <id>321</id><name>Film</name>
-             * <id>49</id><name>Games, Hobbies</name>
              * <id>192</id><name>Lifestyle Shows, Expos</name>
              * <id>1</id><name>Performing Arts</name>
-             * <id>305</id><name>Creative</name>
-             * <id>275</id><name>Dance Classes</name>
-             * <id>163</id><name>Education</name>
-             * <id>140</id><name><![CDATA[Family & Lifestyle]]></name>
-             * <id>205</id><name>Games, Carnivals</name>
-             * <id>126</id><name>Hiking, Camping</name>
-             * <id>60</id><name>Ballet</name>
-             * <id>253</id><name>Cabaret, Burlesque</name>
-             * <id>251</id><name>Choir, Vocal Music</name>
-             * <id>320</id><name>Circus</name>
              * <id>48</id><name>Comedy</name>
              * <id>22</id><name>Dance</name>
-             * <id>25</id><name>Film</name>
+             * <id>60</id><name>Ballet</name>
              * <id>304</id><name>Magic, Variety</name>
              * <id>23</id><name>Musicals</name>
-                  
+             * <id>251</id><name>Choir, Vocal Music</name>
+             * <id>253</id><name>Cabaret, Burlesque</name>
+             * <id>320</id><name>Circus</name>
+             * family 3
+             * <id>5</id><name>Children, Kids, Holidays</name>
+             * <id>52</id><name>Family Entertainment</name>
+             * <id>49</id><name>Games, Hobbies</name>
+             * <id>305</id><name>Creative</name>
+             * <id>140</id><name><![CDATA[Family & Lifestyle]]></name>
+             * <id>205</id><name>Games, Carnivals</name>
+             * Festivals and Carnivals 4
+             * <id>30</id><name>Festivals</name>
+             * <id>205</id><name>Games, Carnivals</name>
+             * Education 5
+             * <id>275</id><name>Dance Classes</name>
+             * <id>163</id><name>Education</name>
+             * Film 6
+             * <id>321</id><name>Film</name>
+             * <id>25</id><name>Film</name>
+             * Hiking and Camping 7
+             * <id>126</id><name>Hiking, Camping</name>
+             * http://api.eventfinda.com.au/v2/events.json?rows=20&order=date&radius=100&point=-37.813611,144.963056&category=23,304,25,22,48,320,251,253,60,11,73,5,52,30,321,49,192,305,275,163,140,205,216
     */
             var username = "familygo2";
             var password = "gr6rmmwzjtwd";
-            HttpWebRequest request = WebRequest.Create("http://api.eventfinda.com.au/v2/events.json?rows=20&order=date&radius=100&point=-37.813611,144.963056&category=23,304,25,22,48,320,251,253,60,11,73,5,52,30,321,49,192,305,275,163,140,205,216") as HttpWebRequest;
+            HttpWebRequest request = WebRequest.Create(basicUrl) as HttpWebRequest;
             request.Method = "GET";
             // Add authentication to request  
             request.Credentials = new NetworkCredential(username, password);
@@ -66,9 +99,16 @@ namespace FamilyGo.Controllers
                 // Console application output  
                 Console.WriteLine(reader.ReadToEnd());
             }
-                return Content(jo.ToString());
+            return Content(jo.ToString());
         }
-
+        public ActionResult GetSuburbs()
+        {
+            List<Suburb> suburbs = db.Suburbs.ToList();
+            StringBuilder stringBuilder = new StringBuilder();
+            JavaScriptSerializer json = new JavaScriptSerializer();
+            json.Serialize(suburbs, stringBuilder);
+            return Content(stringBuilder.ToString());
+        }
 
     }
 }
