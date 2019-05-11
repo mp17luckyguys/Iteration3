4c5a0785899619a4fe1f0e9f12fc88ac3aeb6289
jcui0003 <jcui0003@student.monash.edu>
2019-05-12 09:37:08 +1000

    new home page and home controller

--- FamilyGo/FamilyGo/Controllers/HomeController.cs
+++ FamilyGo/FamilyGo/Controllers/HomeController.cs
@@ -18,7 +18,6 @@ namespace FamilyGo.Controllers
         public ActionResult Index()
         {
             ViewBag.IFrameSrc = "https://melkidsthrive.shinyapps.io/migrant/";
-            ViewBag.IFrameSrc1 = "https://gemmacc.shinyapps.io/exe2/";
             return View();
         }
 
